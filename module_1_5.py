adb5450015e120391a2d36c2af3cb607356a2208
PopsAlex <alexpops3000@gmail.com>
2024-08-06 21:01:07 +0400

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (type, 3, True, "что-то")
+print('Immutable tuple: ', immutable_var)
+# immutable_var[1] = 2
+# print(immutable_var)
+mutable_list = [1, 3, 'hello']
+mutable_list[0] = 15
+print('Mutable list: ', mutable_list)
